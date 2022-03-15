4c6d426a926aa7dd14dcd4291d9e0f820124921e
amelie-elm <amelie.elmquist@gmail.com>
2022-03-14 19:57:10 -0500

    add battle functions

--- Assets/Scripts/Units/Soldier.cs
+++ Assets/Scripts/Units/Soldier.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Soldier : Unit
 {
     public int damage = 5;
+    public bool battleReady = false;
+    public bool hunkeredDown = false;
     public override void Battle(Unit other)
     {
-        throw new System.NotImplementedException();
+        if(!other.battleReady) {
+            // bonus damage!
+            other.ReceiveDamage((int) Math.floor(damage * 1.5));
+        } else if(other.hunkeredDown) {
+            // reduce damage
+            other.ReceiveDamage((int) Math.floor(damage * 0.5));
+        } else {
+            other.ReceiveDamage(damage);
+        }
     }
 
     public override void ReceiveDamage(int damage)
     {
-        throw new System.NotImplementedException();
+        health -= damage;
     }
 
     public override void MoveInternal(int x, int y)
