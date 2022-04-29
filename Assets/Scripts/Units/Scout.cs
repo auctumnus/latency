8f61519eb401ae12882606e33407654784aaeda5
thea0033 <ilikebread1132@gmail.com>
2022-04-28 20:22:58 -0500

    polishing for build

--- Assets/Scripts/Units/Scout.cs
+++ Assets/Scripts/Units/Scout.cs
@@ -1,8 +1,4 @@
 ﻿public class Scout : Soldier
 {
-    public int staminaCapacity = 2;
-    public int currentStamina = 2;
-
-    public int damage = 2;
-    public int health = 5;
+    // this is an empty class, for good reason. 
 }
